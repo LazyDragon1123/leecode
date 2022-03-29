8e7c56a62b08121792c3e88c302306210bc740b4
LazyDragon1123 <lightwalkwithme@gmail.com>
2022-03-29 00:31:19 -0400

    upd

--- python/twentytwo_generate.py
+++ python/twentytwo_generate.py
@@ -1,6 +1,20 @@
-from itertools import combinations
+from shutil import register_archive_format
+
+
+def cand_generate(res, cand, on, cn, n):
+    if on == n and cn ==n:
+        res.append(cand)
+        return
+    if on < n:
+        cand_generate(res, cand + '(', on + 1, cn, n)
+    if cn < on:
+        cand_generate(res, cand + ')', on, cn +1, n)
 
 
 class Solution:
-    def genrateParenthesis(self, n: int) -> List[str]:
-        
+    def generateParenthesis(self, n: int) -> List[str]:
+        # recursion
+        res = []
+        cand_generate(res, cand, on, cn, n)
+        return res
+    
